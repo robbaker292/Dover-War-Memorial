a8bf0f76f782a6c94f0d8f53c0fdc2ed823a9f6c
robbaker292 <robbaker292@gmail.com>
2017-02-12 20:26:06 +0000

    Casualty details no longer DB dump

--- application/views/casualty_view.php
+++ application/views/casualty_view.php
@@ -4,6 +4,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 include APPPATH . 'third_party/Parsedown.php';
 include APPPATH . 'third_party/ParsedownExtra.php';
 $Parsedown = new ParsedownExtra();
+
 ?>
 
 	<h2><?php 
@@ -24,16 +25,40 @@ $Parsedown = new ParsedownExtra();
 	<div id="toggler">Click for more details <i class="fa fa-arrow-circle-down" aria-hidden="true"></i></div>
 	<br>
 	<div id="data">
+
+	<div class="panel panel-primary">
+		<div class="panel-heading">Casualty Details</div>
+		<table class="table table-striped">
 		<?php
 			if($casualty_data->recently_uploaded == 1) {
 				?>
-				<div class="alert alert-info" role="alert"><i class="fa fa-info-circle" aria-hidden="true"></i>&nbsp;This casualty has recently been imported, so full data may not yet be available.</div>
+				<tr class="info"><td colspan="2"><i class="fa fa-info-circle" aria-hidden="true"></i>&nbsp;This casualty has recently been imported, so full data may not yet be available.</td></tr>
 				<?php
 			}
 		?>
-	<table>
+	
+		<tr><th class="col-sm-3">Given Name</th><td><?php echo $casualty_data->given_name; ?></td></tr>
+		<tr><th>Middle Names</th><td><?php echo $casualty_data->middle_names; ?></td></tr>
+		<tr><th>Family Name</th><td><?php echo $casualty_data->family_name; ?></td></tr>
+		<tr><th>Civilian</th><td><?php echo $casualty_data->civilian == 0 ? "Yes" : "No" ; ?></td></tr>
+		<tr><th>Gender</th><td><?php echo $casualty_data->gender == "M" ? "Male" : "Female" ; ?></td></tr>
+		<tr><th>Date of Birth</th><td><?php echo $casualty_data->date_of_birth; ?></td></tr>
+		<tr><th>Date of Death</th><td><?php echo $casualty_data->date_of_death; ?></td></tr>
+		<tr><th>Final Resting Place</th><td><?php echo $casualty_data->final_resting_place; ?></td></tr>
+		<tr><th>Rank at Death</th><td><?php echo $casualty_data->rank; ?></td></tr>
+		<tr><th>War</th><td><?php echo $casualty_data->war; ?></td></tr>
+		<tr><th>Country</th><td><?php echo $casualty_data->country;
+			if($casualty_data->country != "") {
+				echo "&nbsp;<img src=\"".asset_url().$casualty_data->flag."\" alt=\"".$casualty_data->country."\">";
+			}
+		?></td></tr>
+		<tr><th>Last Known Address</th><td><?php echo $casualty_data->last_known_address; ?></td></tr>
+		<tr><th>Last Known Address Year</th><td><?php echo $casualty_data->last_known_address_year; ?></td></tr>
 	<?php
-		foreach ($casualty_data as $key => $value) {
+
+
+
+	/*	foreach ($casualty_data as $key => $value) {
 			if($key == "flag") {
 				continue;
 			}
@@ -51,30 +76,30 @@ $Parsedown = new ParsedownExtra();
 			}		
 			echo "</td>";
 			echo "</tr>";
-		}
+		}*/
 		foreach ($commemorations as $data) {
 			echo "<tr>";
-			echo "<td>";
-			echo "commemorations";
-			echo "</td><td>";
+			echo "<th>";
+			echo "Commemoration Location";
+			echo "</th><td>";
 			echo $data->name;
 			echo "</td>";
 			echo "</tr>";
 		}
 		foreach ($regiment_data as $data) {
 			echo "<tr>";
-			echo "<td>";
-			echo "name";
-			echo "</td><td>";
+			echo "<th>";
+			echo "Regiment / Service Name";
+			echo "</th><td>";
 			echo $data->name;
 			echo "</td>";
 			echo "</tr>";
 		}
 		foreach ($service_numbers as $data) {
 			echo "<tr>";
-			echo "<td>";
-			echo "service number";
-			echo "</td><td>";
+			echo "<th>";
+			echo "Service Number";
+			echo "</th><td>";
 			echo $data->service_number;
 			echo "</td>";
 			echo "</tr>";
@@ -82,5 +107,7 @@ $Parsedown = new ParsedownExtra();
 
 	?>
 	</table>
+
+	</div>
 	</div>
 
