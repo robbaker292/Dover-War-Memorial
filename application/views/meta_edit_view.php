19b6b23a561abe5b1244e9f55e16146cf4a3ffc5
robbaker292 <robbaker292@gmail.com>
2017-05-18 20:45:08 +0100

    Change log added to meta pages

--- application/views/meta_edit_view.php
+++ application/views/meta_edit_view.php
@@ -51,6 +51,14 @@ $Parsedown = new ParsedownExtra();
 		</div>
 
 		<div class="form-group">
+			<label class="control-label col-sm-3" for="changed_details">What's changed:</label>
+			<div class="col-sm-9">
+				<input type="text" class="form-control" id="changed_details" placeholder="Enter details of what has changed in this edit" name="changed_details" value="">
+				<div class="help-block">Leave blank to stop this being listed on the changes page (e.g. admin pages).</div>
+			</div>
+		</div>
+
+		<div class="form-group">
 			<div class="btn-group" role="group" aria-label="...">
 				<button type="button" class="btn btn-primary" id="saveBasic">Save meta page</button>
 			</div>
